62b268e5f757565bac77ff5806f876c882f063df
Nuwan Chamara <NuwanChamP02@gmail.com>
2025-06-06 19:18:33 +0000

    add a webpack and build a static files of this website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Add this line to enable static HTML export
   typescript: {
     ignoreBuildErrors: true,
   },
